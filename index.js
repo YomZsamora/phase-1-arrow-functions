09cd03f385d0602c7c0594ba3f6fbe518a1e7741
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 18:03:32 +0300

    Lab on two different styles for declaring functions: function expressions and arrow functions.

---
+++ index.js
@@ -0,0 +1,8 @@
+// a functional expression called divide
+let divide = () => 2000 / 100
+
+// An arrow function called square
+let square = x => x * x
+
+// An arrow function called add
+let add = (parameter1, parameter2) => parameter1 + parameter2
